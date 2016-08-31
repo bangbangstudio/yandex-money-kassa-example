ad8d58c4d7b93d9a8c5dc303e30b7f8f89e7f871
yushi <tttptd@users.noreply.github.com>
2016-09-01 01:49:24 +0300

    Update checkOrder.php

--- src/checkOrder.php
+++ src/checkOrder.php
@@ -2,9 +2,6 @@
 
 namespace shop;
 
-require_once "YaMoneyCommonHttpProtocol.php";
-require_once "Settings.php";
-
 $settings = new Settings();
 $yaMoneyCommonHttpProtocol = new YaMoneyCommonHttpProtocol("checkOrder", $settings);
 $yaMoneyCommonHttpProtocol->processRequest($_REQUEST);
